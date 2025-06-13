029fda1fc8808107b008062f371a246664944f1b
Miryoqib Sobirov <miryoqibcoder@gmail.com>
2025-06-13 09:15:27 +0500

    feat(knight): реализовано движение и поедание коня

--- src/core/Pieces/Knight.js
+++ src/core/Pieces/Knight.js
@@ -5,4 +5,30 @@ export class Knight extends Piece {
         super(color);
         this.type = "knight";
     }
+
+    canMove(fromY, fromX, toY, toX) {
+        const directionX = Math.abs(fromX - toX);
+        const directionY = Math.abs(fromY - toY);
+
+        const isLshape = directionX === 1 && directionY === 2 || directionX === 2 && directionY === 1;
+
+        if (isLshape) {
+            return true;
+        };
+
+        return false;
+    }
+
+    canEat(fromY, fromX, toY, toX, target) {
+        const directionX = Math.abs(fromX - toX);
+        const directionY = Math.abs(fromY - toY);
+
+        const isLshape = directionX === 1 && directionY === 2 || directionX === 2 && directionY === 1;
+
+        if (isLshape && target && target.color !== this.color) {
+            return true;
+        };
+
+        return false;
+    }
 }
