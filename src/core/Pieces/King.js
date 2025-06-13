2be2c9e2a0448f0162787be0caa9a73b9117329b
Miryoqib Sobirov <miryoqibcoder@gmail.com>
2025-06-13 23:03:37 +0500

    feat(king): реализовано движение и поедание короля

--- src/core/Pieces/King.js
+++ src/core/Pieces/King.js
@@ -5,4 +5,17 @@ export class King extends Piece {
         super(color);
         this.type = "king";
     }
+
+    canMove(fromY, fromX, toY, toX, boardLogic) {
+        const directionX = Math.abs(fromX - toX);
+        const directionY = Math.abs(fromY - toY);
+
+        return directionX <= 1 && directionY <= 1 && (directionX + directionY > 0);
+    }
+
+    canEat(fromY, fromX, toY, toX, target, boardLogic) {
+        if (!target || target.color === this. color) return false;
+
+        return this.canMove(fromY, fromX, toY, toX, boardLogic);
+    }
 }
