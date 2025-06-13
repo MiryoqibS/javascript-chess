bf71d8f4eb3185f973a88f9e29624dde0a36a899
Miryoqib Sobirov <miryoqibcoder@gmail.com>
2025-06-13 10:50:51 +0500

    feat(bishop): реализовано движение и поедание слона

--- src/core/Pieces/Bishop.js
+++ src/core/Pieces/Bishop.js
@@ -5,4 +5,36 @@ export class Bishop extends Piece {
         super(color);
         this.type = "bishop";
     }
+
+    canMove(fromY, fromX, toY, toX, boardLogic) {
+        const deltaX = toX - fromX;
+        const deltaY = toY - fromY;
+
+        if (Math.abs(deltaX) !== Math.abs(deltaY)) {
+            return false;
+        };
+
+        const stepY = deltaY > 0 ? 1 : -1;
+        const stepX = deltaX > 0 ? 1 : -1;
+
+        let y = fromY + stepY;
+        let x = fromX + stepX;
+
+        while (x !== toX && y !== toY) {
+            if (!boardLogic.isEmpty(y, x)) {
+                return false;
+            };
+
+            x += stepX;
+            y += stepY;
+        };
+
+        return true;
+    }
+
+    canEat(fromY, fromX, toY, toX, target, boardLogic) {
+        if (!target || target.color === this.color) return false;
+
+        return this.canMove(fromY, fromX, toY, toX, boardLogic);
+    }
 }
